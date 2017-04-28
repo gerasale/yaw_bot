49d82d6e25b45e228551cca9412baa442fa1f550
Alexander <gerasale@gmail.com>
2017-04-29 02:08:23 +0300

    weather by request added

--- bot.py
+++ bot.py
@@ -8,11 +8,14 @@ bot = telebot.TeleBot(config.token)
 markup = types.ReplyKeyboardMarkup()
 markup.row('Погода в Москве')
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def message(message):
-	bot.send_message(message.chat.id, weather_data.write_weather(2), reply_markup=markup)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, 'Напишите название города, чтобы получить текущую погоду')
 
+@bot.message_handler(content_types=['text'])
+def mes(message):
+	bot.send_message(message.chat.id, weather_data.write_weather(message.text))
 
 
 if __name__ == '__main__':
-	bot.polling(none_stop=True)
+     bot.polling(none_stop=True)
